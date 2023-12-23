3812cf0d2626308ad5e69d3c7c0c2c53d63e3416
mostak9 <mostak.ahm263@gmail.com>
2023-12-23 11:43:05 +0600

    create client side with basic setup with react router, tailwindcss, material tailwind, firebase

---
+++ task-management-client/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react";
+
+
+const Home = () => {
+    return (
+        <div className="text-5xl">
+            This is homepage
+            <Button>Button</Button>
+        </div>
+    );
+};
+
+export default Home;
---
+++ task-management-client/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes/Routes.jsx'
+import { ThemeProvider } from '@material-tailwind/react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider>
+    <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
