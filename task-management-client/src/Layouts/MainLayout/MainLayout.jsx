8dd4bc52627c36d563ff34dbdd89914b2782287b
mostak9 <mostak.ahm263@gmail.com>
2023-12-23 12:01:42 +0600

    added a navgiation bar

--- task-management-client/src/Layouts/MainLayout/MainLayout.jsx
+++ task-management-client/src/Layouts/MainLayout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navigation from "../../Components/Shared/Navigation/Navigation";
 
 
 const MainLayout = () => {
     return (
         <div>
+            <Navigation/>
             <Outlet/>
         </div>
     );
