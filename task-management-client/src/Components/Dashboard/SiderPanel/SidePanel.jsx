17c926272d7c750f838c8ca63220d3ba05c43c85
mostak9 <mostak.ahm263@gmail.com>
2023-12-24 12:20:37 +0600

    added close open functionality fo sidepanel

--- task-management-client/src/Components/Dashboard/SiderPanel/SidePanel.jsx
+++ task-management-client/src/Components/Dashboard/SiderPanel/SidePanel.jsx
@@ -1,63 +1,65 @@
 import {
-    Card,
-    Typography,
-    List,
-    ListItem,
-    ListItemPrefix,
-    ListItemSuffix,
-  } from "@material-tailwind/react";
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 const SidePanel = () => {
-    return (
-        <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
-      <div className="mb-2 p-4">
-        <Typography variant="h5" color="blue-gray">
-          Sidebar
-        </Typography>
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <Button className="md:hidden" onClick={() => setOpen(true)}>
+        SidePanel
+      </Button>
+
+      <div
+        className={`h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5  transform ${
+          open ? "translate-x-0" : "-translate-x-80 md:translate-x-0"
+        } duration-1000 absolute top-0 md:static z-10  rounded-none bg-white`}
+      >
+        <IconButton variant="text" onClick={() => setOpen(false)} className="md:hidden"><IoMdClose className="text-xl font-bold"/></IconButton>
+        <div className="mb-2 p-4 relative flex-1">
+          <Typography variant="h5" color="blue-gray">
+            Sidebar
+          </Typography>
+        </div>
+        <List className="flex-1">
+          <ListItem>
+            <ListItemPrefix></ListItemPrefix>
+            Dashboard
+          </ListItem>
+          <ListItem>
+            <ListItemPrefix></ListItemPrefix>
+            E-Commerce
+          </ListItem>
+          <ListItem>
+            <ListItemPrefix></ListItemPrefix>
+            Inbox
+            <ListItemSuffix></ListItemSuffix>
+          </ListItem>
+          <ListItem>
+            <ListItemPrefix></ListItemPrefix>
+            Profile
+          </ListItem>
+          <ListItem>
+            <ListItemPrefix></ListItemPrefix>
+            Settings
+          </ListItem>
+          <ListItem>
+            <ListItemPrefix></ListItemPrefix>
+            Log Out
+          </ListItem>
+        </List>
       </div>
-      <List>
-        <ListItem>
-          <ListItemPrefix>
-            
-          </ListItemPrefix>
-          Dashboard
-        </ListItem>
-        <ListItem>
-          <ListItemPrefix>
-            
-          </ListItemPrefix>
-          E-Commerce
-        </ListItem>
-        <ListItem>
-          <ListItemPrefix>
-            
-          </ListItemPrefix>
-          Inbox
-          <ListItemSuffix>
-           
-          </ListItemSuffix>
-        </ListItem>
-        <ListItem>
-          <ListItemPrefix>
-            
-          </ListItemPrefix>
-          Profile
-        </ListItem>
-        <ListItem>
-          <ListItemPrefix>
-            
-          </ListItemPrefix>
-          Settings
-        </ListItem>
-        <ListItem>
-          <ListItemPrefix>
-            
-          </ListItemPrefix>
-          Log Out
-        </ListItem>
-      </List>
-    </Card>
-    );
+    </div>
+  );
 };
 
 export default SidePanel;
--- task-management-client/src/Layouts/Dashboard/Dashboard.jsx
+++ task-management-client/src/Layouts/Dashboard/Dashboard.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import SidePanel from "../../Components/Dashboard/SiderPanel/SidePanel";
-
+import { Button } from "@material-tailwind/react";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <SidePanel/>
-        </div>
-    );
+  
+  return (
+    <div className="relative">
+      
+      <div className="absolute top-1/2 left-0"></div>
+      <div className={`md:grid md:grid-cols-4`}>
+        <SidePanel />
+        <div className="md:col-span-3">This is main panel</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
