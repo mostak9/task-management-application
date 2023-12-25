062822fe6d2deeaed1c77193493025eff78945f2
mostak9 <mostak.ahm263@gmail.com>
2023-12-25 16:48:31 +0600

    update footer

--- task-management-client/src/Components/Shared/Footer/Footer.jsx
+++ task-management-client/src/Components/Shared/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import { Typography } from "@material-tailwind/react";
-
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram , FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -21,19 +22,13 @@ const Footer = () => {
 
           <ul className="flex space-x-4">
             <li>
-              <a href="#" className="hover:text-white">
-                Home
-              </a>
+              <a href="#" className="hover:text-white">Home</a>
             </li>
             <li>
-              <a href="#" className="hover:text-white">
-                About
-              </a>
+              <a href="#" className="hover:text-white">About</a>
             </li>
             <li>
-              <a href="#" className="hover:text-white">
-                Features
-              </a>
+              <a href="#" className="hover:text-white">Features</a>
             </li>
             {/* Add more navigation links as needed */}
           </ul>
@@ -41,7 +36,20 @@ const Footer = () => {
 
         <hr className="my-4 border-gray-600" />
 
-        <div className="text-center text-sm">
+        <div className="flex items-center justify-center space-x-4">
+          <a href="#" className="text-gray-300 hover:text-white">
+          <FaSquareXTwitter className="text-2xl" />
+          </a>
+          <a href="#" className="text-gray-300 hover:text-white">
+          <FaInstagram className="text-2xl" />
+          </a>
+          <a href="#" className="text-gray-300 hover:text-white">
+          <FaLinkedin className="text-2xl" />
+          </a>
+         
+        </div>
+
+        <div className="text-center text-sm mt-4">
           <p>&copy; {new Date().getFullYear()} Task Forge. All Rights Reserved.</p>
           <p>Designed and developed with React.js and Tailwind CSS</p>
         </div>
